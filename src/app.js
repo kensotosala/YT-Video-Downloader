868b8cb81957db3efd7e3a5fa1cd147e9f759cf5
kendevtech <kendallsalazarsoto@gmail.com>
2023-06-19 16:19:04 -0600

    19/06/2023

--- src/app.js
+++ src/app.js
@@ -1,22 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const formElement = document.querySelector(".download-form");
-  const inputElement = document.getElementById("input-download");
+const videoInput = document.getElementById("input-download");
+const downloadBtn = document.getElementById("start-btn");
 
-  formElement.addEventListener("submit", async (event) => {
-    event.preventDefault(); // Evitar el comportamiento predeterminado de envío del formulario
+// Replace 'YOUR_API_KEY' with your YouTube Data API key
+const API_KEY = "AIzaSyC08JJw92NnDE5TW6AxThLIFGKm1j1u3TM";
 
-    const inputValue = inputElement.value;
-    console.log(inputValue);
+// Function to fetch video details from YouTube Data API
+function fetchVideoDetails(videoId) {
+  const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`;
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch video details.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.items.length === 0) {
+        throw new Error("Video not found.");
+      }
+      return data.items[0].snippet;
+    });
+}
 
-    // Simulación de una operación asincrónica
-    await delay(1000); // Espera de 1 segundo
+// Function to generate a download link for the video
+function generateDownloadLink(videoId, title) {
+  const url = `https://www.youtube.com/watch?v=${videoId}`;
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = `${title}.mp4`; // Change the file extension if needed
+  link.innerHTML = "Click here to download";
+  document.body.appendChild(link);
+}
 
-    // Limpiar el campo de entrada después de imprimir el valor
-    inputElement.value = "";
-  });
+// Event listener for download button click
+downloadBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const videoId = videoInput.value.trim(); // Get the video ID from the input element
 
-  // Función de utilidad para simular una espera asincrónica
-  function delay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
+  fetchVideoDetails(videoId)
+    .then((snippet) => generateDownloadLink(videoId, snippet.title))
+    .catch((error) => console.error("Error:", error.message));
 });
